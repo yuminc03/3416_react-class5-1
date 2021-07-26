4e6dffacdd4782d0a025add1c1f4282cf2d5d3e2
yuminc03 <yuminc03@gamil.com>
2021-07-26 22:08:15 +0900

    2021-07-26: state는 반드시 setState를 이용하여 값을 변경한다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(props){
+    super(props);
+    console.log('Constructor');
+  }
+
+  state = {
+    count: 0,
+  }
+  render() {
+    console.log('Render');
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>+1</button>
+        <button onClick={this.minus}>-1</button>
+      </div>
+    )
+  };
+};
 
 export default App;
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   state = {
+//     count: 0,
+//   };
+
+//   add = () => {
+//     //console.log("add");
+//     this.setState(current => ({
+//       count: current.count + 1,
+//     }));
+//   };
+
+//   minus = () => {
+//     //console.log("minus");
+//     this.setState(current => ({
+//       count: current.count - 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>The number is: {this.state.count}</h1>
+//         <button onClick={this.add}>+1</button>
+//         <button onClick={this.minus}>-1</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
